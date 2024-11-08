578a4e8370e6020d3471cecd5d0edc9c3e4bcdef
Altuğ ALTINTAŞ <altug.altintas59@gmail.com>
2024-11-08 21:14:23 +0300

    a

--- Proje_web/Controllers/AccountController.cs
+++ Proje_web/Controllers/AccountController.cs
@@ -82,7 +82,7 @@ namespace Proje_web.Controllers
 
             return Json(dTO);
         }
-
+                //deneme
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)  //kişinin ulaşmak istediği sayfa 
         {
